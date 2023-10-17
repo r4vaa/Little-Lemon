89a8422be69209c664e0d870c0c0dd12b6627815
r4vaa <patildheeraj56@gmail.com>
2023-10-17 19:59:57 +0530

    adding date feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+   const [date, setDate] = useState(new Date())
+
+   useEffect(() => {
+      setInterval(() => setDate(new Date()), 1000)
+   })
    return (
       <div className="App">
-         <h1>HomePage</h1>
+         <h1>{date.toLocaleTimeString()}</h1>
       </div>
    );
 }
